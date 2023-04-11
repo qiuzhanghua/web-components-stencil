93c5f1f9ef26d018b74d7bc3d885ee842cc9d595
qiuzhanghua <qiuzhanghua@icloud.com>
2023-04-11 19:25:13 +0800

    use ref

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -7,13 +7,16 @@ import {AV_API_KEY} from "../../global/global";
 })
 export class StockPrice {
 
+  stockInput: HTMLInputElement;
+
   @Element() el: HTMLElement;
 
   @State() fetchedPrice: number;
 
   onFetchStockPrice(event: Event) {
     event.preventDefault();
-    const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    const stockSymbol = this.stockInput.value;
     fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`)
       .then(res => {
         if (res.status !== 200) {
@@ -31,7 +34,7 @@ export class StockPrice {
   render() {
     return [
       <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input id="stock-symbol"/>
+        <input id="stock-symbol" ref = {el => this.stockInput = el}/>
         <button type="submit">Fetch</button>
       </form>,
       <div>
